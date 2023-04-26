14950e3e70ed13b672cf474802997255e3dd6b00
Mariana Abdias <desenvolvimento18@dl.com.br>
2023-04-26 08:19:25 -0300

    atualização do projeto, correção da chamada da api

--- html5-qrcode-react-main/src/ResultContainerPlugin.jsx
+++ html5-qrcode-react-main/src/ResultContainerPlugin.jsx
@@ -1,122 +1,73 @@
 import React, { useEffect, useState } from "react";
+import BeatLoader from "react-spinners/BeatLoader";
 import axios from "axios";
 
 function ResultContainerPlugin(props) {
+  const [loading, setLoading] = useState(false);
   const [dbConsult, setDbConsult] = useState({
-    data: {
-      IMEI1: "",
-      IMEI2: "",
-      SERIAL_NUMBER: "",
-      NAME: "",
-      TYPE: "",
-    },
+    SERIAL_NUMBER: "",
+    NAME: "",
     message: "",
   });
 
-  function consultDB(imeiSerial) {
-    const baseURL =
-      process.env.REACT_APP_BASE_ENDPOINT_CHECKMI +
-      "/product/fromsite/" +
-      imeiSerial.results[0];
+  async function consultDB(imeiSerial) {
+    try {
+      setLoading(true);
+      const baseURL = `${process.env.REACT_APP_BASE_ENDPOINT_CHECKMI}/product/fromsite/${imeiSerial.results[0]}`;
 
-    axios
-      .get(baseURL, {
+      const response = await axios.get(baseURL, {
         headers: {
           "API-KEY": process.env.REACT_APP_API_TOKEN,
         },
-      })
-      .then((response) => {
-        if (
-          imeiSerial.results[0] !== undefined &&
-          response.data !== null &&
-          response.data !== undefined &&
-          Object.keys(response.data).length > 0
-        ) {
-          console.log(imeiSerial.results[0])
-          setDbConsult(response.data);
-        } else {
-          this.responseMessage(
-            "Erro no retorno da" + "requisição. Tente novamente"
-          );
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        switch (err.response.data.statusCode) {
-          case 401:
-            console.log(" ___________________________ ");
-            console.log("|      NÃO AUTORIZADO.      |");
-            console.log("|      TENTE NOVAMENTE      |");
-            console.log("|---------------------------|");
-            console.log("|     STATUS_ERROR: 401     |");
-            console.log("|      ConsultIMEI.tsx      |");
-            console.log(" ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾ ");
-            this.responseMessage("Não autorizado." + "Tente novamente.");
-            break;
-          case 429:
-            console.log(" ___________________________ ");
-            console.log("|  EXCESSO DE REQUISIÇÕES,  |");
-            console.log("|  AGUARDE PARA CONTINUAR.  |");
-            console.log("|---------------------------|");
-            console.log("|     STATUS_ERROR: 429     |");
-            console.log("|      ConsultIMEI.tsx      |");
-            console.log(" ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾ ");
-            this.responseMessage(
-              "Excesso de requisições," + " aguarde para continuar."
-            );
-            break;
-          case 500:
-            console.log(" ___________________________ ");
-            console.log("|   SERVIDOR INDISPONÍVEL   |");
-            console.log("|     OU EM MANUTENÇÃO.     |");
-            console.log("|---------------------------|");
-            console.log("|     STATUS_ERROR: 429     |");
-            console.log("|      ConsultIMEI.tsx      |");
-            console.log(" ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾ ");
-            this.responseMessage("Servidor indisponível" + "ou em manutenção.");
-            break;
-        }
       });
+      if (
+        response.data !== null &&
+        response.data !== undefined &&
+        Object.keys(response.data).length > 0
+      ) {
+        setDbConsult({
+          SERIAL_NUMBER: response.data.data.SERIAL_NUMBER || "",
+          NAME: response.data.data.NAME || "",
+          message: response.data.message,
+        });
+      }
+    } catch (error) {
+      this.responseMessage(
+        `Erro no retorno da requisição. Tente novamente, ${error}`
+      );
+    } finally {
+      setLoading(false);
+    }
   }
 
   useEffect(() => {
-    consultDB(props);
-  }, []);
+    if (props.results[0] !== undefined) consultDB(props);
+  }, [props]);
 
-  if (dbConsult["data"]["SERIAL_NUMBER"] === "") {
-    return null;
-  }
-
-    return (
-      <div>
-        {dbConsult["data"]["SERIAL_NUMBER"] > "" ? (
-          <div className="Barcoder-Result">
-            {dbConsult["data"]["SERIAL_NUMBER"] === undefined ||
-            dbConsult["data"]["SERIAL_NUMBER"] === "" ||
-            dbConsult["data"]["SERIAL_NUMBER"] === null ? (
-              <div></div>
-            ) : (
-              <div>
-                <h1>Serial: {dbConsult["data"]["SERIAL_NUMBER"]}</h1>
-                <div class="alert alert-success" role="alert">
-                  {dbConsult["message"]}
-                  <br />
-                  {dbConsult["data"]["NAME"]}
-                </div>
-              </div>
-            )}
+  return (
+    <div>
+      <BeatLoader color="#000000" loading={loading} css="" />
+      {dbConsult.SERIAL_NUMBER !== "" && !loading ? (
+        <div className="Barcoder-Result">
+          <h1>Serial: {dbConsult.SERIAL_NUMBER}</h1>
+          <div class="alert alert-success" role="alert">
+            {dbConsult.message}
+            <br />
+            {dbConsult.NAME}
           </div>
-        ) : (
-          <div className="Barcoder-Result">
-            <h1>Serial: {props.results[0]}</h1>
-            <div class="alert alert-danger" role="alert">
-              {dbConsult["message"]}
-            </div>
+        </div>
+      ) : dbConsult.message !== "" && !loading ? (
+        <div className="Barcoder-Result">
+          <h1>Serial: {props.results[0]}</h1>
+          <div class="alert alert-danger" role="alert">
+            {dbConsult.message}
           </div>
-        )}
-      </div>
-    );
-
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 }
 
 export default ResultContainerPlugin;
